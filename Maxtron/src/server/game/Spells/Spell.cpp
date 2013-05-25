e8c3e21d128945069e30f5040a1d2bb573e8b7b9
Maxtron <hezekiah@europe.com>
2013-05-25 23:38:44 +0300

    Revert "Fuck rbac #2 & fix charge/intercept delay"
    This reverts commit 7ef2449bd44fbec9f6302d932e92857c3d149cc4.

--- Maxtron/src/server/game/Spells/Spell.cpp
+++ Maxtron/src/server/game/Spells/Spell.cpp
@@ -5947,7 +5947,7 @@ uint32 Spell::GetCCDelay(SpellInfo const* _spell)
 
     case SPELLFAMILY_WARRIOR:
         if (_spell->Mechanic == MECHANIC_STUN)
-            return 63;
+            return CCDELAY;
         break;
 
     case SPELLFAMILY_PRIEST:
