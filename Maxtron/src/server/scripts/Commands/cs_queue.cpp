f95ac259a9a739830e475dd23d93793b8479445d
Maxtron <hezekiah@europe.com>
2013-06-01 01:27:37 +0300

    queue command fix

--- Maxtron/src/server/scripts/Commands/cs_queue.cpp
+++ Maxtron/src/server/scripts/Commands/cs_queue.cpp
@@ -14,15 +14,15 @@ public:
     {
         static ChatCommand arenaCommandTable[] =
         {
-            { "2v2",    SEC_PLAYER,        false,    &Handle2v2Command,        "Queue's for 2v2", NULL },
-            { "3v3",    SEC_PLAYER,        false,    &Handle3v3Command,        "Queue's for 3v3", NULL },
-            { "5v5",    SEC_PLAYER,        false,    &Handle5v5Command,        "Queue's for 5v5", NULL },
-            {  NULL,             0,        false,                 NULL,                       "", NULL }
+            { "2v2",    SEC_PLAYER,        false,    &Handle2v2Command,        "Queue 2v2", NULL },
+            { "3v3",    SEC_PLAYER,        false,    &Handle3v3Command,        "Queue 3v3", NULL },
+            { "5v5",    SEC_PLAYER,        false,    &Handle5v5Command,        "Queue 5v5", NULL },
+            {  NULL,             0,        false,                 NULL,                 "", NULL }
         };
         static ChatCommand commandTable[] =
         {
             { "queue",    SEC_PLAYER,        true,                 NULL,                       "", arenaCommandTable},
-            {    NULL,             0,       false,                 NULL,                       "",               NULL }
+            {    NULL,             0,       false,                 NULL,                       "",             NULL }
         };
         return commandTable;
     }
@@ -31,27 +31,42 @@ public:
     {
         Player* me = handler->GetSession()->GetPlayer();
         if(me->GetArenaTeamId(1) != 0)
+        {
             me->AddBattlegroundQueueId(BATTLEGROUND_QUEUE_2v2);
+        }
         else
+        {
             return true;
+        }
+        return true;
     }
 
     static bool Handle3v3Command(ChatHandler* handler, const char* args)
     {
         Player* me = handler->GetSession()->GetPlayer();
         if(me->GetArenaTeamId(1) != 0)
+        {
             me->AddBattlegroundQueueId(BATTLEGROUND_QUEUE_3v3);
+        }
         else
+        {
             return true;
+        }
+        return true;
     }
 
     static bool Handle5v5Command(ChatHandler* handler, const char* args)
     {
         Player* me = handler->GetSession()->GetPlayer();
         if(me->GetArenaTeamId(1) != 0)
-            me->AddBattlegroundQueueId(BATTLEGROUND_QUEUE_5v5};
+        {
+            me->AddBattlegroundQueueId(BATTLEGROUND_QUEUE_5v5);
+        }
         else
+        {
             return true;
+        }
+        return true;
     }
 };
 
