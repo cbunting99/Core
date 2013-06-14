aa3f57d717069f4f64691dca0e6c831119693b54
Tranquillien <j0sk3m@gmail.com>
2013-06-14 15:05:36 +0200

    Worldchat

--- Maxtron/src/server/scripts/Custom/Worldchat.cpp
+++ Maxtron/src/server/scripts/Custom/Worldchat.cpp
@@ -21,71 +21,71 @@ public:
 			switch (player->getFaction())
 			{
 		case ALLIANCE:
-			snprintf(message, 1024, "|FF9900[MG]|cff0000EE[Alliance]|cffffff00[[%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cff0000EE[Alliance]|cffffff00[[%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		case HORDE:
-			snprintf(message, 1024, "|FF9900[MG]|cffCC1100[Horde]|cffffff00[%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffCC1100[Horde]|cffffff00[%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		default:
-			snprintf(message, 1024, "|FF9900[MG]|cffffff00[NULL][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffffff00[NULL][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 			}
 
 		case SEC_PREMIUM:
-			snprintf(message, 1024, "|FF9900[MG]|cff00ccff[Premium][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cff00ccff[Premium][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 	
 		case SEC_MODERATOR:
-			snprintf(message, 1024, "|FF9900[MG]|cffADFF2F[Moderator][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffADFF2F[Moderator][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_TICKET_GM:
-			snprintf(message, 1024, "|FF9900[MG]|cffADFF2F[GM][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffADFF2F[GM][%s]: %s", player->GetName().c_str(), msg);
 		    sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_EVENT_GM:
-			snprintf(message, 1024, "|FF9900[MG]|cffADFF2F[EM][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffADFF2F[EM][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_GAMEMASTER:
-			snprintf(message, 1024, "|FF9900[MG]|cffADFF2F[Gamemaster][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffADFF2F[Gamemaster][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_HEAD_GM:
-			snprintf(message, 1024, "|FF9900[MG]|cffADFF2F[Head GM][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffADFF2F[Head GM][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_DEVELOPER:
-			snprintf(message, 1024, "|FF9900[MG]|cff8B8B83[Developer][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cff8B8B83[Developer][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_HEAD_DEVELOPER:
-			snprintf(message, 1024, "|FF9900[MG]|cff8B8B83[Head Dev][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cff8B8B83[Head Dev][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_ADMINISTRATOR:
-			snprintf(message, 1024, "|FF9900[MG]|cffFF4500[Administrator][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffFF4500[Administrator][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 
 		case SEC_OWNER:
-			snprintf(message, 1024, "|FF9900[MG]|cffFF4500[Owner][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffFF4500[Owner][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		
 		case SEC_CONSOLE:
-			snprintf(message, 1024, "|FF9900[MG]|cffFF4500[Console][%s]: %s", player->GetName().c_str(), msg);
+			snprintf(message, 1024, "|cffff9900[MG]|cffFF4500[Console][%s]: %s", player->GetName().c_str(), msg);
 			sWorld->SendGlobalText(message, NULL);
 			break;
 		}
